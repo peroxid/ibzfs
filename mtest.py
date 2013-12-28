b5e373f3e3f145e76270a17150777f6e8587b089
Patrick Hirt <patrick.hirt@gmail.com>
2013-12-28 19:28:26 +0100

    fixed type on mtest

--- mtest.py
+++ mtest.py
@@ -18,7 +18,7 @@ def main():
     _file.write(0, 'blah blah blah')
     print 'Saving file to /usr/local/bin/file'
     fs.write(_file, '/usr/local/bin/file')
-    print 'Deleting /usr/local/bin/file')
+    print 'Deleting /usr/local/bin/file'
     fs.delete('/usr/local/bin/file')
     print 'Deleting /usr recursively'
     fs.delete_recursive('/usr')
